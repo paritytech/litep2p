85d607ca456fd234f37a81341a06ceb54cc4253c
Aaro Altonen <a.altonen@hotmail.com>
2023-06-17 22:31:18 +0300

    tests/conformance: Add conformance test for IPFS Ping
    Verify that the IPFS Ping protocol works between litep2p and rust-libp2p

---
+++ tests/conformance/rust_ping.rs
@@ -0,0 +1,123 @@
+// Copyright 2018 Parity Technologies (UK) Ltd.
+// Copyright 2023 litep2p developers
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+use futures::{Stream, StreamExt};
+use libp2p::{
+    identity, ping,
+    swarm::{keep_alive, NetworkBehaviour, SwarmBuilder, SwarmEvent},
+    PeerId, Swarm,
+};
+use litep2p::{
+    crypto::ed25519::Keypair,
+    new::{Litep2p, SupportedProtocol},
+    new_config::Litep2pConfigBuilder,
+    protocol::libp2p::new_ping::{Config as PingConfig, PingEvent},
+    transport::tcp_new::config::TransportConfig as TcpTransportConfig,
+};
+
+/// Our network behaviour.
+///
+/// For illustrative purposes, this includes the [`KeepAlive`](keep_alive::Behaviour) behaviour so a continuous sequence of
+/// pings can be observed.
+#[derive(NetworkBehaviour, Default)]
+struct Behaviour {
+    keep_alive: keep_alive::Behaviour,
+    ping: ping::Behaviour,
+}
+
+// initialize litep2p with ping support
+async fn initialize_litep2p() -> (Litep2p, Box<dyn Stream<Item = PingEvent> + Send>) {
+    let keypair = Keypair::generate();
+    let (ping_config, ping_event_stream) = PingConfig::new(3);
+    let litep2p = Litep2p::new(
+        Litep2pConfigBuilder::new()
+            .with_keypair(keypair)
+            .with_tcp(TcpTransportConfig {
+                listen_address: "/ip6/::1/tcp/0".parse().unwrap(),
+            })
+            .with_ping_protocol(ping_config)
+            .build(),
+    )
+    .await
+    .unwrap();
+
+    (litep2p, ping_event_stream)
+}
+
+fn initialize_libp2p() -> Swarm<Behaviour> {
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+
+    tracing::debug!("Local peer id: {local_peer_id:?}");
+
+    let transport = libp2p::tokio_development_transport(local_key).unwrap();
+    let mut swarm =
+        SwarmBuilder::with_tokio_executor(transport, Behaviour::default(), local_peer_id).build();
+
+    swarm.listen_on("/ip6/::1/tcp/0".parse().unwrap()).unwrap();
+
+    swarm
+}
+
+#[tokio::test]
+async fn libp2p_dials() {
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .try_init();
+
+    let mut libp2p = initialize_libp2p();
+    let (mut litep2p, _ping_event_stream) = initialize_litep2p().await;
+    let address = litep2p.listen_address(SupportedProtocol::Tcp).unwrap();
+
+    libp2p.dial(address).unwrap();
+
+    tokio::spawn(async move {
+        loop {
+            let _ = litep2p.poll_next().await;
+        }
+    });
+
+    loop {
+        tokio::select! {
+            event = libp2p.select_next_some() => {
+                match event {
+                    SwarmEvent::NewListenAddr { address, .. } => {
+                        tracing::info!("Listening on {address:?}")
+                    }
+                    SwarmEvent::Behaviour(BehaviourEvent::Ping(_)) => break,
+                    _ => {}
+                }
+            }
+            _ = tokio::time::sleep(std::time::Duration::from_secs(5)) => {
+                assert!(false, "failed to receive ping in time");
+            }
+        }
+    }
+}
+
+#[tokio::test]
+async fn litep2p_dials() {}
+
+#[tokio::test]
+async fn libp2p_doesnt_support_ping() {}
+
+#[tokio::test]
+async fn litep2p_doesnt_support_ping() {}
