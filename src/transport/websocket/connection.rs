a1021ab6107d0803ba74c3190668be925cf5ff6a
Aaro Altonen <a.altonen@hotmail.com>
2023-07-25 20:57:28 +0300

    transport/websocket: Implement noise negotiation

--- src/transport/websocket/connection.rs
+++ src/transport/websocket/connection.rs
@@ -21,18 +21,26 @@
 #![allow(unused)]
 
 use crate::{
+    codec::unsigned_varint::UnsignedVarint,
     config::Role,
-    crypto::noise::{self, Encrypted, NoiseConfiguration},
-    multistream_select::{dialer_select_proto, listener_select_proto, Negotiated, Version},
+    crypto::{
+        noise::{self, Encrypted, NoiseConfiguration, STATIC_KEY_DOMAIN},
+        PublicKey,
+    },
+    error::{Error, NegotiationError},
+    multistream_select::{Message as MultiStreamMessage, Protocol},
+    peer_id::PeerId,
     protocol::ProtocolSet,
     transport::TransportContext,
     types::{protocol::ProtocolName, ConnectionId},
 };
 
-use futures::{stream::TryStreamExt, AsyncRead, AsyncWrite, Stream, StreamExt};
+use bytes::BytesMut;
+use futures::{stream::TryStreamExt, AsyncRead, AsyncWrite, SinkExt, Stream, StreamExt};
+use prost::Message as _;
 use tokio::{io::AsyncReadExt, net::TcpStream};
 use tokio_tungstenite::{accept_async, tungstenite::protocol::Message, WebSocketStream};
-use tokio_util::io::StreamReader;
+use tokio_util::{compat::FuturesAsyncWriteCompatExt, io::StreamReader};
 use tokio_util::{
     compat::{
         Compat, FuturesAsyncReadCompatExt, TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt,
@@ -46,6 +54,12 @@ use std::{
     task::{Context, Poll},
 };
 
+mod schema {
+    pub(super) mod noise {
+        include!(concat!(env!("OUT_DIR"), "/noise.rs"));
+    }
+}
+
 /// Logging target for the file.
 const LOG_TARGET: &str = "websocket::connection";
 
@@ -54,79 +68,6 @@ pub(crate) struct WebSocketConnection {}
 
 impl WebSocketConnection {
     /// Negotiate protocol.
-    async fn negotiate_protocol<S: AsyncRead + AsyncWrite + Unpin>(
-        stream: S,
-        role: &Role,
-        protocols: Vec<&str>,
-    ) -> crate::Result<(Negotiated<S>, ProtocolName)> {
-        tracing::trace!(target: LOG_TARGET, ?protocols, "negotiating protocols");
-
-        let (protocol, socket) = match role {
-            Role::Dialer => dialer_select_proto(stream, protocols, Version::V1).await?,
-            Role::Listener => listener_select_proto(stream, protocols).await?,
-        };
-
-        tracing::trace!(target: LOG_TARGET, ?protocol, "protocol negotiated");
-
-        Ok((socket, ProtocolName::from(protocol.to_string())))
-    }
-
-    /// Negotiate noise + yamux for the connection.
-    async fn negotiate_connection(
-        stream: TcpStream,
-        connection_id: ConnectionId,
-        context: TransportContext,
-        noise_config: NoiseConfiguration,
-        address: SocketAddr,
-    ) -> crate::Result<Self> {
-        tracing::trace!(
-            target: LOG_TARGET,
-            role = ?noise_config.role,
-            "negotiate connection",
-        );
-
-        let stream = TokioAsyncReadCompatExt::compat(stream).into_inner();
-        let stream = TokioAsyncWriteCompatExt::compat_write(stream);
-
-        let role = noise_config.role;
-        let noise_config = NoiseConfiguration::new(&context.keypair, Role::Listener);
-
-        // negotiate `noise`
-        let (stream, _) =
-            Self::negotiate_protocol(stream, &noise_config.role, vec!["/noise"]).await?;
-
-        tracing::trace!(
-            target: LOG_TARGET,
-            "`multistream-select` and `noise` negotiated"
-        );
-
-        // perform noise handshake
-        let (stream, peer) = noise::handshake(stream.inner(), noise_config).await?;
-        tracing::trace!(target: LOG_TARGET, "noise handshake done");
-        let stream: Encrypted<Compat<TcpStream>> = stream;
-
-        // negotiate `yamux`
-        let (stream, _) = Self::negotiate_protocol(stream, &role, vec!["/yamux/1.0.0"]).await?;
-        tracing::trace!(target: LOG_TARGET, "`yamux` negotiated");
-
-        let connection =
-            yamux::Connection::new(stream.inner(), yamux::Config::default(), role.into());
-        let (control, connection) = yamux::Control::new(connection);
-        let context = ProtocolSet::from_transport_context(peer, context).await?;
-
-        todo!();
-        // Ok(Self {
-        //     peer,
-        //     context,
-        //     control,
-        //     connection,
-        //     _connection_id: connection_id,
-        //     next_substream_id: 0usize,
-        //     pending_substreams: FuturesUnordered::new(),
-        //     address: socket_addr_to_multi_addr(&address),
-        // })
-    }
-
     /// Accept WebSocket connection.
     pub(crate) async fn accept_connection(
         stream: TcpStream,
@@ -141,30 +82,186 @@ impl WebSocketConnection {
             "connection received, negotiate protocols"
         );
 
-        while let Some(value) = stream.next().await {
-            tracing::info!(target: LOG_TARGET, "read: {value:?}");
+        // negotiate noise
+        loop {
+            match stream.next().await {
+                Some(Ok(Message::Binary(value))) => {
+                    tracing::info!(
+                        target: LOG_TARGET,
+                        "decoded: {:?}",
+                        std::str::from_utf8(&value)
+                    );
+                    match MultiStreamMessage::decode(value.into()) {
+                        Ok(MultiStreamMessage::Protocols(protocols)) => {
+                            if protocols.len() != 2 {
+                                todo!();
+                            }
+
+                            let multistream = ProtocolName::from("/multistream/1.0.0");
+                            let noise = ProtocolName::from("/noise");
+
+                            if protocols[0].as_ref() == multistream.as_bytes() {
+                                if protocols[1].as_ref() == noise.as_bytes() {
+                                    tracing::error!(target: LOG_TARGET, "VALID PROTOCOL");
+
+                                    let mut bytes = BytesMut::with_capacity(128);
+                                    let message = MultiStreamMessage::Header(
+                                        crate::multistream_select::HeaderLine::V1,
+                                    );
+                                    let _ = message.encode(&mut bytes).unwrap();
+
+                                    // TODO: no unwraps
+                                    let mut header = UnsignedVarint::encode(bytes).unwrap();
+
+                                    let mut proto_bytes = BytesMut::with_capacity(128);
+                                    let message =
+                                        MultiStreamMessage::Protocol(protocols[1].clone());
+                                    let _ = message.encode(&mut proto_bytes).unwrap();
+                                    let proto_bytes = UnsignedVarint::encode(proto_bytes).unwrap();
+
+                                    header.append(&mut proto_bytes.into());
+
+                                    stream.send(Message::Binary(header)).await.unwrap();
+                                    break;
+                                }
+                            }
+
+                            tracing::info!(
+                                target: LOG_TARGET,
+                                ?protocols,
+                                "respond to multistream-select"
+                            );
+                        }
+                        event => tracing::warn!(target: LOG_TARGET, "unhandled event {event:?}"),
+                    }
+                }
+                event => todo!("unhandled event 2 {event:?}"),
+            }
         }
 
-        // let stream = TokioAsyncReadCompatExt::compat(ws_stream).into_inner();
-        // let stream = TokioAsyncWriteCompatExt::compat_write(stream);
-        // let stream = stream.into_async_read();
-        // let stream =
-        // let noise_config = NoiseConfiguration::new(&context.keypair, Role::Listener);
-        // let role = noise_config.role;
+        tracing::info!(target: LOG_TARGET, "noise negotiated");
 
-        // // negotiate `noise`
-        // let (stream, _) =
-        //     Self::negotiate_protocol(stream, &noise_config.role, vec!["/noise"]).await?;
+        let noise = snow::Builder::new("Noise_XX_25519_ChaChaPoly_SHA256".parse().unwrap());
+        let keypair = noise.generate_keypair().unwrap();
+        let mut noise = noise
+            .local_private_key(&keypair.private)
+            .build_responder()
+            .unwrap();
 
-        // tracing::trace!(
-        //     target: LOG_TARGET,
-        //     "`multistream-select` and `noise` negotiated"
-        // );
+        let noise_payload = schema::noise::NoiseHandshakePayload {
+            identity_key: Some(PublicKey::Ed25519(context.keypair.public()).to_protobuf_encoding()),
+            identity_sig: Some(
+                context
+                    .keypair
+                    .sign(&[STATIC_KEY_DOMAIN.as_bytes(), keypair.public.as_ref()].concat()),
+            ),
+            ..Default::default()
+        };
+        let mut payload = Vec::with_capacity(noise_payload.encoded_len());
+        noise_payload
+            .encode(&mut payload)
+            .expect("Vec<u8> provides capacity as needed");
 
-        // while let Some(event) = ws_stream.next().await {
-        //     tracing::info!(target: LOG_TARGET, ?event, "event received")
-        // }
+        // TODO: ignore random message
+        let _ = stream.next().await;
 
+        loop {
+            match stream.next().await {
+                Some(Ok(Message::Binary(message))) => {
+                    tracing::info!(target: LOG_TARGET, "message len {}", message.len());
+                    let mut buffer = vec![0u8; 1024];
+
+                    match noise.read_message(&message, &mut buffer) {
+                        Ok(len) => buffer.truncate(len),
+                        Err(error) => tracing::error!(
+                            target: LOG_TARGET,
+                            "failed to decode initial handshake"
+                        ),
+                    }
+
+                    let mut buffer = vec![0u8; 1024];
+                    let payload = match noise.write_message(&payload, &mut buffer) {
+                        Ok(len) => {
+                            buffer.truncate(len);
+                            let size = len as u16;
+                            let mut size = size.to_be_bytes().to_vec();
+                            size.append(&mut buffer);
+
+                            size
+                        }
+                        Err(error) => {
+                            tracing::error!(
+                                target: LOG_TARGET,
+                                ?error,
+                                "failed to write noise message"
+                            );
+                            todo!();
+                        }
+                    };
+
+                    stream.send(Message::Binary(payload)).await.unwrap();
+
+                    let _ = stream.next().await.unwrap();
+                    let message = stream.next().await.unwrap();
+
+                    match message {
+                        Ok(Message::Binary(message)) => {
+                            let mut buffer = vec![0u8; 1024];
+                            match noise.read_message(&message, &mut buffer) {
+                                Ok(len) => buffer.truncate(len),
+                                Err(error) => tracing::error!(
+                                    target: LOG_TARGET,
+                                    ?error,
+                                    "failed to read second noise message"
+                                ),
+                            }
+                            let bytes: bytes::Bytes = buffer.into();
+
+                            let peer_id = match schema::noise::NoiseHandshakePayload::decode(bytes)
+                            {
+                                Ok(payload) => {
+                                    tracing::info!(target: LOG_TARGET, "got peer public key");
+                                    let public_key = PublicKey::from_protobuf_encoding(
+                                        &payload.identity_key.ok_or(Error::NegotiationError(
+                                            NegotiationError::PeerIdMissing,
+                                        ))?,
+                                    )?;
+                                    Ok(PeerId::from_public_key(&public_key))
+                                }
+                                Err(err) => {
+                                    tracing::error!(
+                                        target: LOG_TARGET,
+                                        "failed to read public key"
+                                    );
+                                    Err(Error::Unknown)
+                                }
+                            };
+
+                            tracing::info!(target: LOG_TARGET, "peer id, maybe: {peer_id:?}");
+                        }
+                        event => tracing::error!(target: LOG_TARGET, "unhandled event {event:?}"),
+                    }
+
+                    break;
+                }
+                event => todo!("unhandled event 3 {event:?}"),
+            }
+        }
+
+        tracing::info!(target: LOG_TARGET, "NOISE FINISHED");
+
+        loop {
+            match stream.next().await {
+                Some(Ok(Message::Binary(message))) => {
+                    tracing::info!(
+                        target: LOG_TARGET,
+                        "handle binary message, len {}",
+                        message.len()
+                    );
+                }
+                event => todo!("unhandled event 4 {event:?}"),
+            }
+        }
         todo!();
     }
 }
--- src/transport/websocket/mod.rs
+++ src/transport/websocket/mod.rs
@@ -18,6 +18,8 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
+#![allow(unused)]
+
 use crate::{
     error::{AddressError, Error},
     peer_id::PeerId,
