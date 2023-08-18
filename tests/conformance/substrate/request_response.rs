501b93e1e8c8a208c3d410aa4ff21f6ad080cdb5
Aaro Altonen <a.altonen@hotmail.com>
2023-08-18 19:31:08 +0300

    tests: Add more conformance tests for Substrate request-response

--- tests/conformance/substrate/request_response.rs
+++ tests/conformance/substrate/request_response.rs
@@ -21,13 +21,14 @@
 use futures::{channel::oneshot, stream::FuturesUnordered, StreamExt};
 use libp2p::{
     identity,
-    swarm::{NetworkBehaviour as NetworkBehaviourDerive, SwarmBuilder, SwarmEvent},
+    swarm::{SwarmBuilder, SwarmEvent},
     PeerId, Swarm,
 };
 use litep2p::{
     config::Litep2pConfigBuilder,
     protocol::request_response::types::{
-        Config as RequestResponseConfig, RequestResponseEvent, RequestResponseHandle,
+        Config as RequestResponseConfig, RequestResponseError, RequestResponseEvent,
+        RequestResponseHandle,
     },
     transport::tcp::config::TransportConfig as TcpTransportConfig,
     Litep2p, Litep2pEvent,
@@ -35,13 +36,12 @@ use litep2p::{
 use sc_network::{
     peer_store::PeerStore,
     request_responses::{
-        IncomingRequest, OutgoingResponse, ProtocolConfig, RequestResponsesBehaviour,
+        IncomingRequest, OutgoingResponse, ProtocolConfig, RequestFailure,
+        RequestResponsesBehaviour,
     },
     types::ProtocolName,
-    IfDisconnected,
+    IfDisconnected, OutboundFailure,
 };
-
-use std::collections::HashSet;
 
 fn initialize_libp2p() -> (
     Swarm<RequestResponsesBehaviour>,
@@ -59,7 +59,7 @@ fn initialize_libp2p() -> (
         fallback_names: Vec::new(),
         max_request_size: 1024,
         max_response_size: 1024 * 1024,
-        request_timeout: std::time::Duration::from_secs(20),
+        request_timeout: std::time::Duration::from_secs(10),
         inbound_queue: Some(tx),
     }];
 
@@ -182,20 +182,256 @@ async fn request_works() {
                     Err(error) => panic!("failed to receive response from peer: {error:?}"),
                 }
             }
-            _ = tokio::time::sleep(std::time::Duration::from_secs(5)) => {
-                panic!("failed to receive ping in time");
+            _ = tokio::time::sleep(std::time::Duration::from_secs(30)) => {
+                panic!("failed to receive request in time");
             }
         }
     }
 }
 
-// TODO: implemement
 #[tokio::test]
-async fn reject_request() {}
+async fn substrate_reject_request() {
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .try_init();
 
-// TODO: implemement
+    let (mut libp2p, peer_store, requests) = initialize_libp2p();
+    let (mut litep2p, mut handle) = initialize_litep2p().await;
+    let address = litep2p.listen_addresses().next().unwrap().clone();
+    let libp2p_peer = *libp2p.local_peer_id();
+
+    tokio::spawn(peer_store.run());
+    libp2p.dial(address).unwrap();
+
+    loop {
+        tokio::select! {
+            event = libp2p.select_next_some() => {
+                match event {
+                    SwarmEvent::NewListenAddr { address, .. } => {
+                        tracing::info!("Listening on {address:?}")
+                    }
+                    event => {
+                        tracing::info!("libp2p: received {event:?}");
+                    }
+                }
+            }
+            event = litep2p.next_event() => match event.unwrap() {
+                Litep2pEvent::ConnectionEstablished { peer, .. } => {
+                    // TODO: zzz
+                    tokio::time::sleep(std::time::Duration::from_millis(200)).await;
+                    handle.send_request(peer, vec![0, 1, 2, 3, 4]).await.unwrap();
+                }
+                event => tracing::info!("litep2p: received {event:?}"),
+            },
+            event = handle.next_event() => match event.unwrap() {
+                RequestResponseEvent::RequestFailed { peer, error, .. } => {
+                    assert_eq!(peer.to_bytes(), libp2p_peer.to_bytes());
+                    assert_eq!(error, RequestResponseError::Rejected);
+                    break;
+                }
+                event => tracing::warn!("unhandle event: {event:?}"),
+            },
+            request = requests.recv() => match request {
+                Ok(request) => {
+                    drop(request);
+                }
+                Err(error) => {
+                    tracing::error!("failed to read reqeuest: {error:?}")
+                }
+            },
+            _ = tokio::time::sleep(std::time::Duration::from_secs(30)) => {
+                panic!("failed to receive request in time");
+            }
+        }
+    }
+}
+
 #[tokio::test]
-async fn request_timeout() {}
+async fn litep2p_reject_request() {
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .try_init();
+
+    let (mut libp2p, peer_store, _) = initialize_libp2p();
+    let (mut litep2p, mut handle) = initialize_litep2p().await;
+    let address = litep2p.listen_addresses().next().unwrap().clone();
+    let litep2p_peer = *litep2p.local_peer_id();
+    let mut pending_responses = FuturesUnordered::new();
+
+    tokio::spawn(peer_store.run());
+    libp2p.dial(address).unwrap();
+
+    loop {
+        tokio::select! {
+            event = libp2p.select_next_some() => {
+                match event {
+                    SwarmEvent::NewListenAddr { address, .. } => {
+                        tracing::info!("Listening on {address:?}")
+                    }
+                    SwarmEvent::ConnectionEstablished { .. } => {
+                        let (tx, rx) = oneshot::channel();
+                        pending_responses.push(rx);
+
+                        libp2p.behaviour_mut().send_request(
+                            &libp2p::PeerId::from_bytes(&litep2p_peer.to_bytes()).unwrap(),
+                            "/request/1",
+                            vec![1, 3, 3, 7],
+                            tx,
+                            IfDisconnected::ImmediateError,
+                        );
+                    }
+                    event => {
+                        tracing::info!("libp2p: received {event:?}");
+                    }
+                }
+            }
+            event = litep2p.next_event() => match event.unwrap() {
+                event => tracing::info!("litep2p: received {event:?}"),
+            },
+            event = handle.next_event() => match event.unwrap() {
+                RequestResponseEvent::RequestReceived {
+                    request_id,
+                    ..
+                } => {
+                    handle.reject_request(request_id).await;
+                }
+                event => tracing::warn!("unhandle event: {event:?}"),
+            },
+            event = pending_responses.select_next_some(), if !pending_responses.is_empty() => {
+                match event {
+                    Ok(response) => {
+                        assert!(std::matches!(response, Err(RequestFailure::Refused)));
+                        break
+                    }
+                    Err(_) => panic!("failed to read response"),
+                }
+            }
+            _ = tokio::time::sleep(std::time::Duration::from_secs(30)) => {
+                panic!("failed to receive request in time");
+            }
+        }
+    }
+}
+
+#[tokio::test]
+async fn substrate_request_timeout() {
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .try_init();
+
+    let (mut libp2p, peer_store, requests) = initialize_libp2p();
+    let (mut litep2p, mut handle) = initialize_litep2p().await;
+    let address = litep2p.listen_addresses().next().unwrap().clone();
+    let libp2p_peer = *libp2p.local_peer_id();
+    let mut _timeout_request = None;
+
+    tokio::spawn(peer_store.run());
+    libp2p.dial(address).unwrap();
+
+    loop {
+        tokio::select! {
+            event = libp2p.select_next_some() => {
+                match event {
+                    SwarmEvent::NewListenAddr { address, .. } => {
+                        tracing::info!("Listening on {address:?}")
+                    }
+                    event => {
+                        tracing::info!("libp2p: received {event:?}");
+                    }
+                }
+            }
+            event = litep2p.next_event() => match event.unwrap() {
+                Litep2pEvent::ConnectionEstablished { peer, .. } => {
+                    // TODO: zzz
+                    tokio::time::sleep(std::time::Duration::from_millis(200)).await;
+                    handle.send_request(peer, vec![0, 1, 2, 3, 4]).await.unwrap();
+                }
+                event => tracing::info!("litep2p: received {event:?}"),
+            },
+            event = handle.next_event() => match event.unwrap() {
+                RequestResponseEvent::RequestFailed { peer, error, .. } => {
+                    assert_eq!(peer.to_bytes(), libp2p_peer.to_bytes());
+                    assert_eq!(error, RequestResponseError::Timeout);
+                    break;
+                }
+                event => tracing::warn!("unhandle event: {event:?}"),
+            },
+            request = requests.recv() => match request {
+                Ok(request) => {
+                    _timeout_request = Some(request);
+                }
+                Err(error) => {
+                    tracing::error!("failed to read reqeuest: {error:?}")
+                }
+            },
+            _ = tokio::time::sleep(std::time::Duration::from_secs(30)) => {
+                panic!("failed to receive request in time");
+            }
+        }
+    }
+}
+
+#[tokio::test]
+async fn litep2p_request_timeout() {
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .try_init();
+
+    let (mut libp2p, peer_store, _) = initialize_libp2p();
+    let (mut litep2p, mut handle) = initialize_litep2p().await;
+    let address = litep2p.listen_addresses().next().unwrap().clone();
+    let litep2p_peer = *litep2p.local_peer_id();
+    let mut pending_responses = FuturesUnordered::new();
+
+    tokio::spawn(peer_store.run());
+    libp2p.dial(address).unwrap();
+
+    loop {
+        tokio::select! {
+            event = libp2p.select_next_some() => {
+                match event {
+                    SwarmEvent::NewListenAddr { address, .. } => {
+                        tracing::info!("Listening on {address:?}")
+                    }
+                    SwarmEvent::ConnectionEstablished { .. } => {
+                        let (tx, rx) = oneshot::channel();
+                        pending_responses.push(rx);
+
+                        libp2p.behaviour_mut().send_request(
+                            &libp2p::PeerId::from_bytes(&litep2p_peer.to_bytes()).unwrap(),
+                            "/request/1",
+                            vec![1, 3, 3, 7],
+                            tx,
+                            IfDisconnected::ImmediateError,
+                        );
+                    }
+                    event => {
+                        tracing::info!("libp2p: received {event:?}");
+                    }
+                }
+            }
+            event = litep2p.next_event() => match event.unwrap() {
+                event => tracing::info!("litep2p: received {event:?}"),
+            },
+            event = handle.next_event() => match event.unwrap() {
+                RequestResponseEvent::RequestReceived { .. } => {},
+                event => tracing::warn!("unhandle event: {event:?}"),
+            },
+            event = pending_responses.select_next_some(), if !pending_responses.is_empty() => {
+                match event {
+                    Ok(response) => {
+                        assert!(std::matches!(response, Err(RequestFailure::Network(OutboundFailure::Timeout))));
+                        break
+                    }
+                    Err(_) => panic!("failed to read response"),
+                }
+            }
+            _ = tokio::time::sleep(std::time::Duration::from_secs(30)) => {
+                panic!("failed to receive request in time");
+            }
+        }
+    }
+}
 
 // TODO: implemement
 #[tokio::test]
