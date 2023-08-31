90936782155ab572c171398dcbfd79be9d8b989e
Aaro Altonen <a.altonen@hotmail.com>
2023-08-31 20:28:50 +0300

    tests: Gate Substrate conformance tests behind a feature flag
    Currently running the tests require some some changes to Substrate
    which have not been upstreamed (and may never be accepted) so hide them
    behind a feature flag so that running all tests won't break because of
    missing changes to Substrate.

--- tests/conformance/substrate/mod.rs
+++ tests/conformance/substrate/mod.rs
@@ -18,6 +18,8 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
+#![cfg(feature = "custom-sc-network")]
+
 #[cfg(test)]
 mod connection;
 #[cfg(test)]
