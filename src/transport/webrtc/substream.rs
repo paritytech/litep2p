0999c0f0ac0cf5565dfbe2d7e8a5e8f9f5d41a1d
Aaro Altonen <a.altonen@hotmail.com>
2023-09-06 21:13:28 +0300

    transport/webrtc: Use `ChannelId` for identifying substreams

--- src/transport/webrtc/substream.rs
+++ src/transport/webrtc/substream.rs
@@ -23,11 +23,11 @@
 use crate::{
     codec::{identity::Identity, unsigned_varint::UnsignedVarint, ProtocolCodec},
     error::Error,
-    types::SubstreamId,
 };
 
 use bytes::BytesMut;
 use futures::{Sink, Stream};
+use str0m::channel::ChannelId;
 use tokio::sync::mpsc::{channel, Receiver, Sender};
 use tokio_stream::wrappers::ReceiverStream;
 use tokio_util::sync::PollSender;
@@ -42,11 +42,11 @@ use std::{
 /// Channel-backed substream.
 #[derive(Debug)]
 pub struct Substream {
-    /// Protocol ID.
-    id: SubstreamId,
+    /// Channel ID.
+    id: ChannelId,
 
     /// TX channel for sending messages to transport.
-    tx: PollSender<(SubstreamId, Vec<u8>)>,
+    tx: PollSender<(ChannelId, Vec<u8>)>,
 
     /// RX channel for receiving messages from transport.
     rx: ReceiverStream<Vec<u8>>,
@@ -57,7 +57,7 @@ pub struct Substream {
 
 impl Substream {
     /// Create new [`Substream`].
-    pub fn new(id: SubstreamId, tx: Sender<(SubstreamId, Vec<u8>)>) -> (Self, Sender<Vec<u8>>) {
+    pub fn new(id: ChannelId, tx: Sender<(ChannelId, Vec<u8>)>) -> (Self, Sender<Vec<u8>>) {
         let (to_protocol, rx) = channel(64);
 
         (
@@ -122,10 +122,10 @@ impl Stream for Substream {
 // TODO: rename?
 pub struct SubstreamBackend {
     /// TX channel for creating new [`Substream`] objects.
-    tx: Sender<(SubstreamId, Vec<u8>)>,
+    tx: Sender<(ChannelId, Vec<u8>)>,
 
     /// RX channel for receiving messages from protocols.
-    rx: Receiver<(SubstreamId, Vec<u8>)>,
+    rx: Receiver<(ChannelId, Vec<u8>)>,
 }
 
 impl SubstreamBackend {
@@ -137,54 +137,12 @@ impl SubstreamBackend {
     }
 
     /// Create new substream.
-    pub fn substream(&mut self, id: SubstreamId) -> (Substream, Sender<Vec<u8>>) {
+    pub fn substream(&mut self, id: ChannelId) -> (Substream, Sender<Vec<u8>>) {
         Substream::new(id, self.tx.clone())
     }
 
     /// Poll next event.
-    pub async fn next_event(&mut self) -> Option<(SubstreamId, Vec<u8>)> {
+    pub async fn next_event(&mut self) -> Option<(ChannelId, Vec<u8>)> {
         self.rx.recv().await
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use futures::{SinkExt, StreamExt};
-
-    #[tokio::test]
-    async fn test_channel_substream() {
-        let mut backend = SubstreamBackend::new();
-        let (mut substream1, sender1) = backend.substream(SubstreamId::from(1usize));
-        let (mut substream2, sender2) = backend.substream(SubstreamId::from(2usize));
-
-        substream1.apply_codec(ProtocolCodec::UnsignedVarint(None));
-        substream2.apply_codec(ProtocolCodec::UnsignedVarint(None));
-
-        substream1.send(bytes::Bytes::from(vec![1, 3, 3, 7])).await.unwrap();
-        substream2.send(bytes::Bytes::from(vec![1, 3, 3, 8])).await.unwrap();
-
-        let event = backend.next_event().await.unwrap();
-        assert_eq!(event.0, SubstreamId::from(1usize));
-        assert_eq!(event.1, UnsignedVarint::encode(vec![1, 3, 3, 7]).unwrap());
-
-        let event = backend.next_event().await.unwrap();
-        assert_eq!(event.0, SubstreamId::from(2usize));
-        assert_eq!(event.1, UnsignedVarint::encode(vec![1, 3, 3, 8]).unwrap());
-
-        sender1.send(vec![0, 1, 2, 3, 4]).await.unwrap();
-        sender2.send(vec![5, 6, 7, 8, 9]).await.unwrap();
-
-        assert_eq!(
-            substream1.next().await.unwrap().unwrap(),
-            BytesMut::from(vec![0, 1, 2, 3, 4].as_slice())
-        );
-        assert_eq!(
-            substream2.next().await.unwrap().unwrap(),
-            BytesMut::from(vec![5, 6, 7, 8, 9].as_slice())
-        );
-    }
-
-    #[tokio::test]
-    async fn close_substream() {}
 }
