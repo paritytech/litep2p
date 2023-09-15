0c18026f6cc8a3cd314ba1da1198a7ce92d99147
Aaro Altonen <a.altonen@hotmail.com>
2023-09-15 23:25:46 +0300

    substream: Introduce new substream type
    The new substream implements framing for sending and receiving using the
    codec that the user provided when the protocol was initialized.

--- src/substream/mod.rs
+++ src/substream/mod.rs
@@ -1,3 +1,4 @@
+// Copyright 2020 Parity Technologies (UK) Ltd.
 // Copyright 2023 litep2p developers
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
@@ -20,11 +21,16 @@
 
 //! Substream-related helper code.
 
-use crate::error::{Error, SubstreamError};
+use crate::{
+    codec::ProtocolCodec,
+    error::{Error, SubstreamError},
+    transport::{quic, tcp, websocket},
+};
 
-use bytes::{Bytes, BytesMut};
+use bytes::{Buf, Bytes, BytesMut};
 use futures::{Sink, Stream};
-use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use unsigned_varint::{decode, encode};
 
 use std::{
     collections::{hash_map::Entry, HashMap},
@@ -34,19 +40,26 @@ use std::{
     task::{Context, Poll},
 };
 
+/// Logging target for the file.
+const LOG_TARGET: &str = "substream";
+
 /// Raw substream received from one of the enabled transports.
+// TODO: remove
 pub trait RawSubstream: AsyncRead + AsyncWrite + Unpin + Send + Debug + 'static {}
 
 /// Blanket implementation for [`RawSubstream`].
+// TODO: remove
 impl<T: AsyncRead + AsyncWrite + Unpin + Send + Debug + 'static> RawSubstream for T {}
 
 /// Trait which describes the behavior of a substream.
+// TODO: remove
 pub trait Substream:
     Debug + Stream<Item = crate::Result<BytesMut>> + Sink<Bytes, Error = Error> + Send + Unpin + 'static
 {
 }
 
 /// Blanket implementation for [`Substream`].
+// TODO: remove
 impl<
         T: Debug
             + Stream<Item = crate::Result<BytesMut>>
@@ -56,6 +69,307 @@ impl<
             + 'static,
     > Substream for T
 {
+}
+
+/// Substream type.
+#[derive(Debug)]
+enum SubstreamType {
+    Tcp(tcp::Substream),
+    WebSocket(websocket::Substream),
+    Quic(quic::Substream),
+}
+
+/// `Litep2p` substream type.
+///
+/// Implements `tokio::io::AsyncRead`/`tokio::io::AsyncWrite` traits which can be wrapped
+/// in a `Framed` to implement a custom codec.
+///
+/// In case a codec for the protocol was specified, `Substream::send()`/Substream::next()`
+/// are also provided which implement the necessary framing to read/write codec-encoded messages
+/// from the underlying socket.
+#[derive(Debug)]
+pub struct NewSubstream {
+    // Inner substream.
+    substream: SubstreamType,
+
+    /// Protocol codec.
+    codec: ProtocolCodec,
+}
+
+impl NewSubstream {
+    /// Create new [`Substream`] for TCP.
+    pub(crate) fn new_tcp(substream: tcp::Substream, codec: ProtocolCodec) -> Self {
+        tracing::trace!(target: LOG_TARGET, ?codec, "create new substream for tcp");
+
+        Self {
+            codec,
+            substream: SubstreamType::Tcp(substream),
+        }
+    }
+    /// Create new [`Substream`] for WebSocket.
+    pub(crate) fn new_websocket(substream: websocket::Substream, codec: ProtocolCodec) -> Self {
+        tracing::trace!(target: LOG_TARGET, ?codec, "create new substream for websocket");
+
+        Self {
+            codec,
+            substream: SubstreamType::WebSocket(substream),
+        }
+    }
+    /// Create new [`Substream`] for QUIC.
+    pub(crate) fn new_quic(substream: quic::Substream, codec: ProtocolCodec) -> Self {
+        tracing::trace!(target: LOG_TARGET, ?codec, "create new substream for quic");
+
+        Self {
+            codec,
+            substream: SubstreamType::Quic(substream),
+        }
+    }
+
+    /// Send framed data to remote peer.
+    ///
+    /// Panics if no codec is provided.
+    pub async fn send(&mut self, bytes: Bytes) -> crate::Result<()> {
+        match &mut self.substream {
+            SubstreamType::Tcp(ref mut substream) => match self.codec {
+                ProtocolCodec::Generic => todo!(),
+                ProtocolCodec::Identity(payload_size) => {
+                    if bytes.len() != payload_size {
+                        return Err(Error::InvalidData);
+                    }
+
+                    substream
+                        .write_all(&bytes)
+                        .await
+                        .map_err(|_| Error::SubstreamError(SubstreamError::ConnectionClosed))
+                }
+                ProtocolCodec::UnsignedVarint(max_size) => {
+                    if let Some(max_size) = max_size {
+                        if bytes.len() > max_size {
+                            return Err(Error::InvalidData);
+                        }
+                    }
+
+                    let len_bytes = BytesMut::zeroed(10);
+                    let mut buffer: [u8; 10] = len_bytes.as_ref().try_into().expect("to succeed");
+                    unsigned_varint::encode::usize(bytes.len(), &mut buffer);
+
+                    let size = len_bytes.len() + bytes.len();
+                    let mut len_bytes = len_bytes.chain(bytes).copy_to_bytes(size);
+
+                    substream
+                        .write_all_buf(&mut len_bytes)
+                        .await
+                        .map_err(|_| Error::SubstreamError(SubstreamError::ConnectionClosed))
+                }
+            },
+            SubstreamType::WebSocket(ref mut substream) => match self.codec {
+                ProtocolCodec::Generic => todo!(),
+                ProtocolCodec::Identity(payload_size) => {
+                    if bytes.len() != payload_size {
+                        return Err(Error::InvalidData);
+                    }
+
+                    substream
+                        .write_all(&bytes)
+                        .await
+                        .map_err(|_| Error::SubstreamError(SubstreamError::ConnectionClosed))
+                }
+                ProtocolCodec::UnsignedVarint(max_size) => {
+                    if let Some(max_size) = max_size {
+                        if bytes.len() > max_size {
+                            return Err(Error::InvalidData);
+                        }
+                    }
+
+                    let len_bytes = BytesMut::zeroed(10);
+                    let mut buffer: [u8; 10] = len_bytes.as_ref().try_into().expect("to succeed");
+                    unsigned_varint::encode::usize(bytes.len(), &mut buffer);
+
+                    let size = len_bytes.len() + bytes.len();
+                    let mut len_bytes = len_bytes.chain(bytes).copy_to_bytes(size);
+
+                    substream
+                        .write_all_buf(&mut len_bytes)
+                        .await
+                        .map_err(|_| Error::SubstreamError(SubstreamError::ConnectionClosed))
+                }
+            },
+            SubstreamType::Quic(ref mut substream) => match self.codec {
+                ProtocolCodec::Generic => todo!(),
+                ProtocolCodec::Identity(payload_size) => {
+                    if bytes.len() != payload_size {
+                        return Err(Error::InvalidData);
+                    }
+
+                    substream.write_all(bytes).await
+                }
+                ProtocolCodec::UnsignedVarint(max_size) => {
+                    if let Some(max_size) = max_size {
+                        if bytes.len() > max_size {
+                            return Err(Error::InvalidData);
+                        }
+                    }
+
+                    let len_bytes = BytesMut::zeroed(10);
+                    let mut buffer: [u8; 10] = len_bytes.as_ref().try_into().expect("to succeed");
+                    unsigned_varint::encode::usize(bytes.len(), &mut buffer);
+
+                    substream.write_all_chunks(&mut [len_bytes.freeze(), bytes]).await
+                }
+            },
+        }
+    }
+
+    /// Read `unsigned-varint` payload size from `io`.
+    async fn read_unsigned_varint_payload_size<T: AsyncRead + Unpin>(io: &mut T) -> Option<usize> {
+        let mut b = encode::usize_buffer();
+        for i in 0..b.len() {
+            let n = io.read(&mut b[i..i + 1]).await.ok()?;
+            if n == 0 {
+                tracing::trace!(target: LOG_TARGET, "substream closed while reading `unsigned-varint` payload size");
+                return None;
+            }
+
+            if decode::is_last(b[i]) {
+                return Some(decode::usize(&b[..=i]).ok()?.0);
+            }
+        }
+
+        tracing::debug!(target: LOG_TARGET, "overflow in `unsigned-varint` payload size");
+        None
+    }
+
+    /// Read `unsigned-varint` payload from `io`.
+    async fn read_unsigned_varint_payload<T: AsyncRead + Unpin>(
+        io: &mut T,
+        max_size: Option<usize>,
+    ) -> Option<Bytes> {
+        let payload_size = Self::read_unsigned_varint_payload_size(io).await?;
+
+        if let Some(max_size) = max_size {
+            if payload_size > max_size {
+                tracing::debug!(target: LOG_TARGET, ?payload_size, ?max_size, "too big payload");
+                return None;
+            }
+        }
+
+        let mut payload = BytesMut::zeroed(payload_size);
+        io.read_exact(&mut payload).await.ok()?;
+
+        Some(payload.freeze())
+    }
+
+    /// Read `Identity` payload from `io`.
+    async fn read_identity_payload<T: AsyncRead + Unpin>(
+        io: &mut T,
+        payload_size: usize,
+    ) -> Option<Bytes> {
+        let mut bytes = BytesMut::zeroed(payload_size);
+        io.read_exact(&mut bytes).await.ok()?;
+        Some(bytes.freeze())
+    }
+
+    /// Read next frame from the underlying socket.
+    ///
+    /// Panics if no codec is provided.
+    pub async fn next(&mut self) -> Option<Bytes> {
+        match &mut self.substream {
+            SubstreamType::Tcp(ref mut substream) => match self.codec {
+                ProtocolCodec::Generic => todo!(),
+                ProtocolCodec::Identity(payload_size) =>
+                    Self::read_identity_payload(substream, payload_size).await,
+                ProtocolCodec::UnsignedVarint(max_size) =>
+                    Self::read_unsigned_varint_payload(substream, max_size).await,
+            },
+            SubstreamType::WebSocket(ref mut substream) => match self.codec {
+                ProtocolCodec::Generic => todo!(),
+                ProtocolCodec::Identity(payload_size) =>
+                    Self::read_identity_payload(substream, payload_size).await,
+                ProtocolCodec::UnsignedVarint(max_size) =>
+                    Self::read_unsigned_varint_payload(substream, max_size).await,
+            },
+            SubstreamType::Quic(ref mut substream) => match self.codec {
+                ProtocolCodec::Generic => todo!(),
+                ProtocolCodec::Identity(payload_size) =>
+                    Self::read_identity_payload(substream, payload_size).await,
+                ProtocolCodec::UnsignedVarint(max_size) =>
+                    Self::read_unsigned_varint_payload(substream, max_size).await,
+            },
+        }
+    }
+}
+
+impl tokio::io::AsyncRead for NewSubstream {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut tokio::io::ReadBuf<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        match &mut self.substream {
+            SubstreamType::Tcp(ref mut substream) => Pin::new(substream).poll_read(cx, buf),
+            SubstreamType::WebSocket(ref mut substream) => Pin::new(substream).poll_read(cx, buf),
+            SubstreamType::Quic(ref mut substream) => Pin::new(substream).poll_read(cx, buf),
+        }
+    }
+}
+
+impl tokio::io::AsyncWrite for NewSubstream {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, std::io::Error>> {
+        match &mut self.substream {
+            SubstreamType::Tcp(ref mut substream) => Pin::new(substream).poll_write(cx, buf),
+            SubstreamType::WebSocket(ref mut substream) => Pin::new(substream).poll_write(cx, buf),
+            SubstreamType::Quic(ref mut substream) => Pin::new(substream).poll_write(cx, buf),
+        }
+    }
+
+    fn poll_flush(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), std::io::Error>> {
+        match &mut self.substream {
+            SubstreamType::Tcp(ref mut substream) => Pin::new(substream).poll_flush(cx),
+            SubstreamType::WebSocket(ref mut substream) => Pin::new(substream).poll_flush(cx),
+            SubstreamType::Quic(ref mut substream) => Pin::new(substream).poll_flush(cx),
+        }
+    }
+
+    fn poll_shutdown(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), std::io::Error>> {
+        match &mut self.substream {
+            SubstreamType::Tcp(ref mut substream) => Pin::new(substream).poll_shutdown(cx),
+            SubstreamType::WebSocket(ref mut substream) => Pin::new(substream).poll_shutdown(cx),
+            SubstreamType::Quic(ref mut substream) => Pin::new(substream).poll_shutdown(cx),
+        }
+    }
+
+    fn poll_write_vectored(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        bufs: &[std::io::IoSlice<'_>],
+    ) -> Poll<Result<usize, std::io::Error>> {
+        match &mut self.substream {
+            SubstreamType::Tcp(ref mut substream) =>
+                Pin::new(substream).poll_write_vectored(cx, bufs),
+            SubstreamType::WebSocket(ref mut substream) =>
+                Pin::new(substream).poll_write_vectored(cx, bufs),
+            SubstreamType::Quic(ref mut substream) =>
+                Pin::new(substream).poll_write_vectored(cx, bufs),
+        }
+    }
+
+    fn is_write_vectored(&self) -> bool {
+        match &self.substream {
+            SubstreamType::Tcp(substream) => substream.is_write_vectored(),
+            SubstreamType::WebSocket(substream) => substream.is_write_vectored(),
+            SubstreamType::Quic(substream) => substream.is_write_vectored(),
+        }
+    }
 }
 
 /// Substream set key.
