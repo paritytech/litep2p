200a0fddc9aaae2ec845cb569eb521f09e536da1
Alexandru Vasile <alexandru.vasile@parity.io>
2024-04-18 17:37:50 +0300

    kad: Include peers that are also not connected to the query
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- src/protocol/libp2p/kademlia/mod.rs
+++ src/protocol/libp2p/kademlia/mod.rs
@@ -743,14 +743,15 @@ impl Kademlia {
 
                             let key = Key::new(record.key.clone());
 
-
                             if let Some(peers) = peers {
                                 // Put the record to the specified peers.
                                 let peers: VecDeque<_> = peers.into_iter().filter_map(|peer| {
-                                    if let KBucketEntry::Occupied(entry) = self.routing_table.entry(Key::from(peer)) {
-                                        Some(entry.clone())
-                                    } else {
-                                        None
+                                    match self.routing_table.entry(Key::from(peer)) {
+                                        // The routing table contains information about the peer address when:
+                                        // - Occupied: Established connection
+                                        // - Vacant: We'll try to establish the connection later, but the address is known.
+                                        KBucketEntry::Occupied(entry) | KBucketEntry::Vacant(entry) => Some(entry.clone()),
+                                        _ => None,
                                     }
                                 }).collect();
 
