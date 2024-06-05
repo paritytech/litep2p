06ebb419a8ff915875d3ece653d056566d1ee0fb
Alexandru Vasile <60601340+lexnv@users.noreply.github.com>
2024-06-05 17:34:07 +0300

    Update src/crypto/noise/mod.rs
    Co-authored-by: Dmitry Markin <dmitry@markin.tech>

--- src/crypto/noise/mod.rs
+++ src/crypto/noise/mod.rs
@@ -169,6 +169,7 @@ impl NoiseContext {
 
         let NoiseState::Handshake(ref mut noise) = self.noise else {
             tracing::error!(target: LOG_TARGET, "invalid state to read the second handshake message");
+            debug_assert!(false);
             return Err(error::Error::Other(
                 "Noise state missmatch: expected handshake".into(),
             ));
