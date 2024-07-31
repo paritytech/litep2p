985ecf9bf003b7319fb816bc6e5072d40be15904
Alexandru Vasile <alexandru.vasile@parity.io>
2024-07-31 11:41:17 +0300

    kad: Do not udpate memory store on incoming GetRecordSuccess
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- src/protocol/libp2p/kademlia/mod.rs
+++ src/protocol/libp2p/kademlia/mod.rs
@@ -656,31 +656,6 @@ impl Kademlia {
                 Ok(())
             }
             QueryAction::GetRecordQueryDone { query_id, records } => {
-                // Considering this gives a view of all peers and their records, some peers may have
-                // outdated records. Store only the record which is backed by most
-                // peers.
-                let now = std::time::Instant::now();
-                let rec = records
-                    .iter()
-                    .filter_map(|peer_record| {
-                        if peer_record.record.is_expired(now) {
-                            None
-                        } else {
-                            Some(&peer_record.record)
-                        }
-                    })
-                    .fold(HashMap::new(), |mut acc, rec| {
-                        *acc.entry(rec).or_insert(0) += 1;
-                        acc
-                    })
-                    .into_iter()
-                    .max_by_key(|(_, v)| *v)
-                    .map(|(k, _)| k);
-
-                if let Some(record) = rec {
-                    self.store.put(record.clone());
-                }
-
                 let _ = self
                     .event_tx
                     .send(KademliaEvent::GetRecordSuccess {
