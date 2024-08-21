de1044ddb5d5f731358741d9d1bdc71056b2dd35
Alexandru Vasile <alexandru.vasile@parity.io>
2024-08-21 11:22:37 +0300

    protocol: Use thiserr
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- src/multistream_select/protocol.rs
+++ src/multistream_select/protocol.rs
@@ -422,21 +422,26 @@ where
 }
 
 /// A protocol error.
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum ProtocolError {
     /// I/O error.
+    #[error("I/O error: `{0}`")]
     IoError(io::Error),
 
     /// Received an invalid message from the remote.
+    #[error("Received an invalid message from the remote.")]
     InvalidMessage,
 
     /// A protocol (name) is invalid.
+    #[error("A protocol (name) is invalid.")]
     InvalidProtocol,
 
     /// Too many protocols have been returned by the remote.
+    #[error("Too many protocols have been returned by the remote.")]
     TooManyProtocols,
 
     /// The protocol is not supported.
+    #[error("The protocol is not supported.")]
     ProtocolNotSupported,
 }
 
@@ -458,26 +463,5 @@ impl From<ProtocolError> for io::Error {
 impl From<uvi::decode::Error> for ProtocolError {
     fn from(err: uvi::decode::Error) -> ProtocolError {
         Self::from(io::Error::new(io::ErrorKind::InvalidData, err.to_string()))
-    }
-}
-
-impl Error for ProtocolError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        match *self {
-            ProtocolError::IoError(ref err) => Some(err),
-            _ => None,
-        }
-    }
-}
-
-impl fmt::Display for ProtocolError {
-    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        match self {
-            ProtocolError::IoError(e) => write!(fmt, "I/O error: {e}"),
-            ProtocolError::InvalidMessage => write!(fmt, "Received an invalid message."),
-            ProtocolError::InvalidProtocol => write!(fmt, "A protocol (name) is invalid."),
-            ProtocolError::TooManyProtocols => write!(fmt, "Too many protocols received."),
-            ProtocolError::ProtocolNotSupported => write!(fmt, "The protocol is not supported."),
-        }
     }
 }
