852f2c9e4b0906c3f7d9a4370ceef5ec27ade9ed
Alexandru Vasile <alexandru.vasile@parity.io>
2025-01-22 14:15:51 +0200

    multistream: Enfore io::error instead of empty protocols
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- src/multistream_select/protocol.rs
+++ src/multistream_select/protocol.rs
@@ -201,8 +201,7 @@ impl Message {
         let mut remaining: &[u8] = &msg;
         loop {
             // A well-formed message must be terminated with a newline.
-            // TODO: don't do this
-            if remaining == [b'\n'] || remaining.is_empty() {
+            if remaining == [b'\n'] {
                 break;
             } else if protocols.len() == MAX_PROTOCOLS {
                 return Err(ProtocolError::TooManyProtocols);
