9d27c1d2b2aed44ef2bf451478e3ad005f31e0eb
Alexandru Vasile <alexandru.vasile@parity.io>
2025-04-14 14:13:58 +0300

    crypto/noise: Set timeout limits for the noise handshake
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- src/crypto/noise/mod.rs
+++ src/crypto/noise/mod.rs
@@ -709,62 +709,71 @@ pub async fn handshake<S: AsyncRead + AsyncWrite + Unpin>(
     role: Role,
     max_read_ahead_factor: usize,
     max_write_buffer_size: usize,
+    timeout: std::time::Duration,
 ) -> Result<(NoiseSocket<S>, PeerId), NegotiationError> {
-    tracing::debug!(target: LOG_TARGET, ?role, "start noise handshake");
+    let handle_handshake = async move {
+        tracing::debug!(target: LOG_TARGET, ?role, "start noise handshake");
 
-    let mut noise = NoiseContext::new(keypair, role)?;
-    let payload = match role {
-        Role::Dialer => {
-            // write initial message
-            let first_message = noise.first_message(Role::Dialer)?;
-            let _ = io.write(&first_message).await?;
-            io.flush().await?;
+        let mut noise = NoiseContext::new(keypair, role)?;
+        let payload = match role {
+            Role::Dialer => {
+                // write initial message
+                let first_message = noise.first_message(Role::Dialer)?;
+                let _ = io.write(&first_message).await?;
+                io.flush().await?;
 
-            // read back response which contains the remote peer id
-            let message = noise.read_handshake_message(&mut io).await?;
-            // Decode the remote identity message.
-            let payload = handshake_schema::NoiseHandshakePayload::decode(message)
+                // read back response which contains the remote peer id
+                let message = noise.read_handshake_message(&mut io).await?;
+                // Decode the remote identity message.
+                let payload = handshake_schema::NoiseHandshakePayload::decode(message)
                 .map_err(ParseError::from)
                 .map_err(|err| {
                     tracing::error!(target: LOG_TARGET, ?err, "failed to decode remote identity message");
                     err
                 })?;
 
-            // send the final message which contains local peer id
-            let second_message = noise.second_message()?;
-            let _ = io.write(&second_message).await?;
-            io.flush().await?;
+                // send the final message which contains local peer id
+                let second_message = noise.second_message()?;
+                let _ = io.write(&second_message).await?;
+                io.flush().await?;
 
-            payload
-        }
-        Role::Listener => {
-            // read remote's first message
-            let _ = noise.read_handshake_message(&mut io).await?;
+                payload
+            }
+            Role::Listener => {
+                // read remote's first message
+                let _ = noise.read_handshake_message(&mut io).await?;
 
-            // send local peer id.
-            let second_message = noise.second_message()?;
-            let _ = io.write(&second_message).await?;
-            io.flush().await?;
+                // send local peer id.
+                let second_message = noise.second_message()?;
+                let _ = io.write(&second_message).await?;
+                io.flush().await?;
 
-            // read remote's second message which contains their peer id
-            let message = noise.read_handshake_message(&mut io).await?;
-            // Decode the remote identity message.
-            handshake_schema::NoiseHandshakePayload::decode(message).map_err(ParseError::from)?
-        }
+                // read remote's second message which contains their peer id
+                let message = noise.read_handshake_message(&mut io).await?;
+                // Decode the remote identity message.
+                handshake_schema::NoiseHandshakePayload::decode(message)
+                    .map_err(ParseError::from)?
+            }
+        };
+
+        let dh_remote_pubkey = noise.get_handshake_dh_remote_pubkey()?;
+        let peer = parse_and_verify_peer_id(payload, dh_remote_pubkey)?;
+
+        Ok((
+            NoiseSocket::new(
+                io,
+                noise.into_transport()?,
+                max_read_ahead_factor,
+                max_write_buffer_size,
+            ),
+            peer,
+        ))
     };
 
-    let dh_remote_pubkey = noise.get_handshake_dh_remote_pubkey()?;
-    let peer = parse_and_verify_peer_id(payload, dh_remote_pubkey)?;
-
-    Ok((
-        NoiseSocket::new(
-            io,
-            noise.into_transport()?,
-            max_read_ahead_factor,
-            max_write_buffer_size,
-        ),
-        peer,
-    ))
+    match tokio::time::timeout(timeout, handle_handshake).await {
+        Err(_) => Err(NegotiationError::Timeout),
+        Ok(result) => result,
+    }
 }
 
 // TODO: https://github.com/paritytech/litep2p/issues/125 add more tests
