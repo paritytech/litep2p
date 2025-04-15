83c651ca0faa0343b6a5068d72dfa3f05f80e8a5
Alexandru Vasile <alexandru.vasile@parity.io>
2025-04-15 12:36:10 +0300

    websocket/tests: Add test for noise_timeout_listener
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- src/transport/websocket/connection.rs
+++ src/transport/websocket/connection.rs
@@ -840,4 +840,77 @@ mod tests {
             Err(error) => panic!("invalid error: {error:?}"),
         }
     }
+
+    #[tokio::test]
+    async fn noise_timeout_listener() {
+        let _ = tracing_subscriber::fmt()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .try_init();
+
+        let listener = TcpListener::bind("[::1]:0").await.unwrap();
+        let address = listener.local_addr().unwrap();
+
+        let (Ok(dialer), Ok((stream, dialer_address))) =
+            tokio::join!(TcpStream::connect(address.clone()), listener.accept(),)
+        else {
+            panic!("failed to establish connection");
+        };
+
+        let keypair = Keypair::generate();
+        let peer_id = PeerId::from_public_key(&keypair.public().into());
+
+        let dialer_address = Multiaddr::empty()
+            .with(Protocol::from(dialer_address.ip()))
+            .with(Protocol::Tcp(dialer_address.port()))
+            .with(Protocol::Ws(std::borrow::Cow::Borrowed("/")))
+            .with(Protocol::P2p(peer_id.into()));
+
+        let (url, peer) = WebSocketTransport::multiaddr_into_url(dialer_address.clone()).unwrap();
+
+        tokio::spawn(async move {
+            // Negotiate websocket.
+            let stream = tokio_tungstenite::client_async_tls(url, dialer).await.unwrap().0;
+            let mut dialer = BufferedStream::new(stream);
+
+            // Sleep while negotiating /yamux.
+            let (stream, _proto) = WebSocketConnection::negotiate_protocol(
+                dialer,
+                &Role::Dialer,
+                vec!["/noise"],
+                std::time::Duration::from_secs(10),
+            )
+            .await
+            .unwrap();
+
+            let (stream, peer) = noise::handshake(
+                stream.inner(),
+                &keypair,
+                Role::Dialer,
+                5,
+                2,
+                std::time::Duration::from_secs(10),
+            )
+            .await
+            .unwrap();
+
+            tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+        });
+
+        match WebSocketConnection::accept_connection(
+            stream,
+            ConnectionId::from(0usize),
+            Keypair::generate(),
+            dialer_address,
+            Default::default(),
+            5,
+            2,
+            Duration::from_secs(10),
+        )
+        .await
+        {
+            Ok(_) => panic!("connection was supposed to fail"),
+            Err(NegotiationError::Timeout) => {}
+            Err(error) => panic!("invalid error: {error:?}"),
+        }
+    }
 }
