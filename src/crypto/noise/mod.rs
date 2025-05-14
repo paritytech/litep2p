3bd9085b405ed2c109610ac402d0aed58b52cd92
Alexandru Vasile <alexandru.vasile@parity.io>
2025-05-14 13:13:54 +0000

    crypto/noise: Add extra debug messages
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- src/crypto/noise/mod.rs
+++ src/crypto/noise/mod.rs
@@ -545,7 +545,7 @@ impl<S: AsyncRead + AsyncWrite + Unpin> AsyncRead for NoiseSocket<S> {
                                 buf,
                             ) {
                                 Err(error) => {
-                                    tracing::error!(target: LOG_TARGET, ?error, "failed to decrypt message");
+                                    tracing::error!(target: LOG_TARGET, ?error, peer = ?this.peer, "failed to decrypt message for bigger buffers");
                                     return Poll::Ready(Err(io::ErrorKind::InvalidData.into()));
                                 }
                                 Ok(nread) => {
@@ -563,7 +563,7 @@ impl<S: AsyncRead + AsyncWrite + Unpin> AsyncRead for NoiseSocket<S> {
                                     &mut buffer,
                                 ) {
                                     Err(error) => {
-                                        tracing::error!(target: LOG_TARGET, ?error, "failed to decrypt message");
+                                        tracing::error!(target: LOG_TARGET, ?error, "failed to decrypt message for smaller buffers");
                                         return Poll::Ready(Err(io::ErrorKind::InvalidData.into()));
                                     }
                                     Ok(nread) => {
