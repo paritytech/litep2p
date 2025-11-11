ae072a8d59fc6f183d0e4268589ba0c173a0f14b
Dmitry Markin <dmitry@markin.tech>
2025-11-11 13:33:31 +0200

    monor: return copyright notice

--- src/protocol/libp2p/kademlia/store.rs
+++ src/protocol/libp2p/kademlia/store.rs
@@ -1,3 +1,5 @@
+// Copyright 2023 litep2p developers
+//
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation
