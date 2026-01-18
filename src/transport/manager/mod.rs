b4e6a3b76f2a0f5a0b950e62e543f581be3e6b20
dharjeezy <dharjeezy@gmail.com>
2026-01-18 18:47:18 +0100

    nit

--- src/transport/manager/mod.rs
+++ src/transport/manager/mod.rs
@@ -348,7 +348,7 @@ impl TransportManager {
             self.next_substream_id.clone(),
             self.transport_manager_handle.clone(),
             keep_alive_timeout,
-            true,
+            false,
         );
 
         self.protocols.insert(
